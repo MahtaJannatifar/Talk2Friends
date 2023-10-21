d297119e4ff3a6336d3ca01b33249f3f0c6a5085
MahtaJannatifar <jannatif@usc.edu>
2023-10-20 19:41:48 -0700

    Email Verification Sent

--- app/src/main/java/com/example/talk2friends/Invitation.java
+++ app/src/main/java/com/example/talk2friends/Invitation.java
@@ -4,10 +4,7 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
-import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.http.javanet.NetHttpTransport;
 import java.io.IOException;
-import java.util.Collections;
 import java.util.Properties;
 import com.google.api.services.gmail.Gmail;
 import javax.mail.MessagingException;
@@ -16,11 +13,8 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-// App pasword: hpwb qoqo mewj dvag
-// To write python scripts to send emails from java
 // TODO: inviting to a specific meeting, make sure to pass meeting ID
 public class Invitation {
-
     private static final String GMAIL_SCOPE = "https://www.googleapis.com/auth/gmail.send";
     private String receiverUserEmail;
     private Auth auth;
@@ -34,14 +28,6 @@ public class Invitation {
         this.auth = auth;
         this.receiverUserEmail = receiverUserEmail;
         Invitation.context = context;
-
-        // Initialize the GoogleAccountCredential and Gmail service
-        credential = GoogleAccountCredential.usingOAuth2(
-                context, Collections.singletonList(GMAIL_SCOPE));
-        credential.setSelectedAccountName(systemEmail);
-        gmail = new Gmail.Builder(new NetHttpTransport(), new GsonFactory(), credential)
-                .setApplicationName("Talk2Friends")
-                .build();
     }
 
     @SuppressLint("StaticFieldLeak")
@@ -52,15 +38,13 @@ public class Invitation {
                 String subject = "Invitation to join a meeting in Talk2Friends";
                 String body = "You've been invited to join a meeting. Please use the following verification code to complete your registration: " + verificationCode;
                 try {
-                    createEmail(receiverUserEmail, systemEmail, subject, body);
+                    createEmail(receiverUserEmail, subject, body);
                     return true;
                 } catch (Exception e) {
-                    Log.e("EMAIL_ERROR", "Exception while sending email: " + e.getMessage());
                     e.printStackTrace();
                     return false;
                 }
             }
-
             @Override
             protected void onPostExecute(Boolean result) {
                 if (result) {
@@ -71,9 +55,6 @@ public class Invitation {
             }
         }.execute();
     }
-
-
-
     private String generateVerificationCode() {
         //Generate a random 6-digit number
         return String.valueOf((int)(Math.random() * 1000000));
@@ -84,22 +65,19 @@ public class Invitation {
             return null;
         }
     }
-
     public void sendInvitationEmail(){
         if(auth.isLoggedIn()){
             // send invite
             String verificationCode =  generateVerificationCode();
             new SendEmailTask().execute(verificationCode);
-            Log.d("CODE IS ", verificationCode);
         }
         else{
             System.out.println("Error, User is not authenticated.");
         }
     }
-
-    public static void createEmail(String to, String from, String subject, String bodyText) throws MessagingException, IOException {
-        final String username = "talk2friends310@gmail.com"; // Your Gmail address
-        final String appPassword = "hpwbqoqomewjdvag"; // Your 16-character App password
+    public static void createEmail(String to, String subject, String bodyText) throws MessagingException, IOException {
+        final String username = "talk2friends310@gmail.com";
+        final String appPassword = context.getString(R.string.APP_PASSWORD);
 
         Properties props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com");
@@ -121,7 +99,6 @@ public class Invitation {
             message.setRecipients(javax.mail.Message.RecipientType.TO, InternetAddress.parse(to));
             message.setSubject(subject);
             message.setText(body);
-
             Transport.send(message);
             System.out.println("Email Sent Successfully");
         } catch (MessagingException e) {
