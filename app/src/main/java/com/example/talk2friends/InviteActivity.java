ebb3250e2ef49ff4ed8a0ebee4e5e7d20eb80f20
MahtaJannatifar <jannatif@usc.edu>
2023-10-29 19:28:06 -0700

    invite page

---
+++ app/src/main/java/com/example/talk2friends/InviteActivity.java
@@ -0,0 +1,48 @@
+package com.example.talk2friends;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class InviteActivity extends AppCompatActivity {
+
+    private String inviteeEmail;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invite);
+    }
+
+    public void sendInvite(View view){
+        Auth auth = new Auth();
+        EditText emailEditText = (EditText) findViewById(R.id.emailText);
+        inviteeEmail = emailEditText.getText().toString();
+        Log.d("EMAILS ", inviteeEmail);
+        // TODO: code will be sent to another user, once that user signs in, need to ask them
+        //  to enter their verification code=> how?
+        Invitation invitation = new Invitation(auth, inviteeEmail, this);
+        String code = invitation.getVerificationCode();
+        String meetingId = "1234";
+        invitation.sendInvitationEmail(meetingId,new Invitation.EmailCallback() {
+            @Override
+            public void onEmailSent(boolean isSuccessful) {
+                if (isSuccessful) {
+                    Log.d("TADA", "sent");
+                    Toast.makeText(InviteActivity.this, "Email sent successfully!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(InviteActivity.this, "Failed to send the email!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    public void goBack(View view){
+        Intent intent = new Intent(this, ViewMeeting.class);
+        startActivity(intent);
+    }
+}
